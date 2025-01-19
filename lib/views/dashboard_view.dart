8a2388ffcc34172032d7826083f85d1da1a28a77
Yahya Almongdie <90466657+yahyaalmongdie@users.noreply.github.com>
2025-01-19 17:44:51 +0300

    Update dashboard_view.dart

--- lib/views/dashboard_view.dart
+++ lib/views/dashboard_view.dart
@@ -1,5 +1,4 @@
 
-
 import 'package:flutter/material.dart';
 import 'package:responcive_dashboard/utils/size_config.dart';
 import 'package:responcive_dashboard/widgets/adaptive_layout_widget.dart';
